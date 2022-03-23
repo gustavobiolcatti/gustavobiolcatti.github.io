8f5013b49575dbfb31b197c2a30d8b5beb051c3a
Gustavo Biolcatti <43222626+gustavobiolcatti@users.noreply.github.com>
2022-03-23 17:38:05 -0300

    reestruturação de código

--- script/index.js
+++ script/index.js
@@ -1,7 +1,9 @@
 const botaoLightmode = document.querySelector(".footer__lightmode")
+const botaoVoltarTopo = document.getElementById("footer__voltar-topo")
 const html = document.querySelector("html")
 
 botaoLightmode.addEventListener("click", () => {
     botaoLightmode.classList.toggle("footer__lightmode--ativo")
+    botaoVoltarTopo.classList.toggle("footer__voltar-topo--ativo")
     html.classList.toggle("light-mode")
 })
